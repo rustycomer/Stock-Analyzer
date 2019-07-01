5279c2938edb1b13f13a7d96a8ce9fcfd43b562b
Russell Comer <rcomer@crimsonquest.com>
2019-06-30 23:12:26 -0400

    Added data fetching, removed api key file from commit. Receives json data from alphavantage for intraday or daily data.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from stock_analyzer_functions import *
+from alphavantage import get_api_key
 
 
-
+symbol = "AAPL"
+data = get_alphavantage_stock_price_daily(symbol, get_api_key())
+print(data)
--- stock_analyzer_functions.py
+++ stock_analyzer_functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime as dt
 
 
 def get_sector_holdings(sector_symbol):
@@ -12,5 +13,25 @@ def get_sector_holdings(sector_symbol):
     return symbols
 
 
-user_input = input("Enter symbol: ")
-get_sector_holdings(user_input)
+def get_start_date():
+    start_date = dt.date.today() - dt.timedelta(days=730)
+    return start_date
+
+
+def get_end_date():
+    end_date = dt.date.today()
+    return end_date
+
+
+def get_alphavantage_stock_price_intraday(symbol, api_key, interval=60):
+    data = pd.read_json(
+        "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={}&interval={}min&apikey={}".format
+        (symbol, interval, api_key))
+    return data
+
+
+def get_alphavantage_stock_price_daily(symbol, api_key):
+    data = pd.read_json(
+        "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&apikey={}".format
+        (symbol, api_key))
+    return data
